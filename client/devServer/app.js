e4a24b7ea9fd76e70c6bedd89be4cd0a189ec963
freum <d.khamchenko@gmail.com>
2018-02-14 16:17:15 +0300

    fix client

--- client/devServer/app.js
+++ client/devServer/app.js
@@ -11,7 +11,7 @@ WebpackMiddlewar(app);
 
 app.use('/', (req, res) => {
   res.set('Content-Type', 'text/html');
-  res.body = fs.readFileSync(path.resolve(__dirname, '../../public/index.html'));
+  res.body = fs.readFileSync(path.resolve(__dirname, '../../../public/index.html'));
 });
 
 export default app;
