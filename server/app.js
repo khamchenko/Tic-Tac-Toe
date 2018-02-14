baf6eff9a29224a5fcc77f9ecff57668e4e9f941
freum <d.khamchenko@gmail.com>
2018-02-14 16:54:53 +0300

    server fix

--- server/app.js
+++ server/app.js
@@ -1,14 +1,10 @@
 import express from 'express';
 const path = require('path');
-import { IS_DEV, IS_PROD } from './utils/env';
+import { IS_PROD } from './utils/env';
 
 import initHandlers from './handlers';
 
 const app = express();
-
-if (IS_DEV) {
-  app.use('/', express.static(path.join(__dirname, '../', 'build')));
-}
 
 if (IS_PROD) {
   app.use('/static', express.static(path.join(__dirname, '..', 'static')));
--- server/modules/index.js
+++ server/modules/index.js
@@ -8,6 +8,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
+import Template from './template';
 import routes from '../../client/src/routes';
 import reducers from '../../client/src/redux/reducers';
 
@@ -35,31 +36,10 @@ export default () => {
 	      </Provider>
 	    );
 	    const initialState = store.getState();
-      res.status(200).send(renderHTML({
+      res.status(200).send(Template({
         content: content,
         initialState: initialState,
       }));
 	  });
 	}
 };
-
-function renderHTML({ content, initialState }) {
-   return `
-       <!DOCTYPE html>
-       <html>
-       <head>
-         <meta charset="utf-8">
-         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <title>Tic Tac Toe</title>
-         <link rel="stylesheet" href="/styles/main.css">
-       </head>
-       <body>
-       <div id="root">${content}</div>
-          <script type="text/javascript">
-           window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-         </script>
-         <script type="text/javascript" src="/app.js"></script>
-       </body>
-       </html>
-   `;
-}
